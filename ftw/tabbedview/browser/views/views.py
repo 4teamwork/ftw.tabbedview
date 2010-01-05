eae4ae86cdf735611bd2e4aa36ae5c1ee8723d95
victor.baumann <victor.baumann@c579a809-fee1-0310-b81e-b0ae3483ba1b>
2010-01-05 12:18:29 +0000

    tabbedview with solr
    git-svn-id: https://svn.4teamwork.ch/repos/ftw/ftw.tabbedview/trunk@32666 c579a809-fee1-0310-b81e-b0ae3483ba1b

--- ftw/tabbedview/browser/views/views.py
+++ ftw/tabbedview/browser/views/views.py
@@ -1,16 +1,26 @@
+from datetime import datetime, timedelta
+import time
+import bisect
 from Products.Five.browser import BrowserView
 from ftw.table import helper
 from Products.CMFCore.utils import getToolByName
-from zope.component import queryMultiAdapter
+from zope.component import getMultiAdapter, queryMultiAdapter
+from zope.component import queryUtility
+from plone.app.content.browser.folderfactories import _allowedTypes
 from Products.CMFCore.Expression import getExprContext
+from Products.statusmessages.interfaces import IStatusMessage
 from zope.component import queryUtility
 from ftw.table.interfaces import ITableGenerator
 from Products.ATContentTypes.interface import IATTopic
 from zope.app.pagetemplate import ViewPageTemplateFile
 from plone.app.content.batching import Batch
 from plone.memoize import instance
+from Acquisition import aq_parent, aq_inner
+from zope.component import queryUtility
+from opengever.globalsolr.interfaces import ISearch
+from collective.solr.flare import PloneFlare
 from Acquisition import aq_inner
-
+ 
 DEFAULT_ENABLED_ACTIONS = [
     'cut',
     'copy',
@@ -19,6 +29,7 @@ DEFAULT_ENABLED_ACTIONS = [
     'delete',
     'change_state',
     ]
+
 
 class TabbedView(BrowserView):
 
@@ -48,10 +59,11 @@ class TabbedView(BrowserView):
                         'class' : ' '.join(view.get_css_classes()),
                         }
 
+
     def selected_tab(self):
         return 'Dokumente'
 
-class BaseListingView(BrowserView):
+class ListingView(BrowserView):
     types = [] #friendly types
     #columns possible values
     # "<attributename>"
@@ -69,14 +81,41 @@ class BaseListingView(BrowserView):
     sort_on = 'sortable_title'
     sort_order = 'reverse'
     search_options = {}
-
     depth = -1
     table = None
     batching = ViewPageTemplateFile("batching.pt")
+    contents = []
 
     def __call__(self):
         self.update()
         return super(BaseListingView, self).__call__()
+
+    def update(self):
+        self.search()
+
+    def search(self, kwargs):
+        pass
+
+    @property
+    @instance.memoize
+    def batch(self):
+        return self.contents
+
+    def show_search_results(self):
+        if self.request.has_key('searchable_text'):
+            searchable_text = self.request.get('searchable_text')
+            return bool(len(searchable_text))
+        return False
+
+    def render_listing(self):
+        generator = queryUtility(ITableGenerator, 'ftw.tablegenerator')
+        return generator.generate(self.batch,
+                                  self.columns,
+                                  sortable=True,
+                                  selected=(self.sort_on, self.sort_order),
+                                  template = self.table
+                                  )
+
 
     def get_css_classes(self):
         if self.show_searchform:
@@ -129,66 +168,6 @@ class BaseListingView(BrowserView):
         enabled = callable(enabled) and list(enabled()) or list(enabled)
         return list(filter(lambda a:a not in major, enabled))
 
-    def render_listing(self):
-        generator = queryUtility(ITableGenerator, 'ftw.tablegenerator')
-        return generator.generate(self.batch,
-                                  self.columns,
-                                  sortable=True,
-                                  selected=(self.sort_on, self.sort_order),
-                                  template = self.table
-                                  )
-
-    def update(self):
-        catalog = getToolByName(self.context,'portal_catalog')
-        self.pas_tool = getToolByName(self.context, 'acl_users')
-        kwargs = {}
-
-        self.pagesize = 20
-        self.pagenumber =  int(self.request.get('pagenumber', 1))
-        self.url = self.context.absolute_url()
-        if len(self.types):
-            kwargs['portal_type'] = self.types
-        else:
-            kwargs['portal_type'] = self.context.getFriendlyTypes()
-
-        if self.request.has_key('searchable_text'):
-            searchable_text = self.request.get('searchable_text')
-            if len(searchable_text):
-                searchable_text = searchable_text.endswith('*') and searchable_text or searchable_text+'*'
-                kwargs['SearchableText'] = searchable_text
-        kwargs['sort_on'] = self.sort_on = self.request.get('sort_on', self.sort_on)
-
-        if self.sort_on.startswith('header-'):
-            kwargs['sort_on'] = self.sort_on = self.sort_on.split('header-')[1]
-
-        kwargs['sort_order'] = self.sort_order = self.request.get('sort_order', self.sort_order)
-
-        if IATTopic.providedBy(self.context):
-            contentsMethod = self.context.queryCatalog
-        else:
-            contentsMethod = self.context.getFolderContents
-
-
-        #overwrite options with search_options dict on tab
-        kwargs.update(self.search_options)
-
-        self.contents = catalog(path=dict(depth=self.depth, query='/'.join(self.context.getPhysicalPath())), **kwargs)
-        #when we're searching recursively we have to remove the current item if its in self.types
-        if self.depth != 1 and self.context.portal_type in self.types:
-            index = 0
-            current_path = '/'.join(self.context.getPhysicalPath())
-            for result in self.contents:
-                if current_path == result.getPath():
-                    self.contents = list(self.contents)
-                    self.contents.pop(index)
-                    #removing from LazyMap doesn't work
-                    #self.contents._len -= 1
-                    #self.contents.actual_result_count -= 1
-                    #res = self.contents._data.pop(index)
-                index +=1
-
-        self.len_results = len(self.contents)
-
     def buttons(self):
         if callable(self.enabled_actions):
             enabled_actions = list(self.enabled_actions())
@@ -201,12 +180,12 @@ class BaseListingView(BrowserView):
         # Do not show buttons if there is no data, unless there is data to be
         # pasted
         if not len(self.contents):
-            if self.context.cb_dataValid():
-                for button in button_actions:
-                    if button['id'] == 'paste':
-                        return [self.setbuttonclass(button)]
-            else:
-                return []
+                if self.context.cb_dataValid():
+                    for button in button_actions:
+                        if button['id'] == 'paste':
+                            return [self.setbuttonclass(button)]
+                else:
+                    return []
 
         for button in button_actions:
             # Make proper classes for our buttons
@@ -236,19 +215,97 @@ class BaseListingView(BrowserView):
         else:
             button['cssclass'] = 'context'
         return button
+        
+
+    def setbuttonclass(self, button):
+        if button['id'] == 'paste':
+            button['cssclass'] = 'standalone'
+        else:
+            button['cssclass'] = 'context'
+        return button
+
+    @property
+    def _search_options(self):
+        for k,v in self.search_options.items():
+            if callable(v):
+                self.search_options[k]=v(self.context)
+        return self.search_options
+
+class BaseListingView(ListingView):
+
+    def update(self):
+        self.pas_tool = getToolByName(self.context, 'acl_users')
+        kwargs = {}
+
+        self.pagesize = 20
+        self.pagenumber =  int(self.request.get('pagenumber', 1))
+        self.url = self.context.absolute_url()
+        if len(self.types):
+            kwargs['portal_type'] = self.types
+        else:
+            kwargs['portal_type'] = self.context.getFriendlyTypes()
+
+        if self.request.has_key('searchable_text'):
+            searchable_text = self.request.get('searchable_text')
+            if len(searchable_text):
+                searchable_text = searchable_text.endswith('*') and searchable_text or searchable_text+'*'
+                kwargs['SearchableText'] = searchable_text
+        kwargs['sort_on'] = self.sort_on = self.request.get('sort_on', self.sort_on)
+
+        if self.sort_on.startswith('header-'):
+            kwargs['sort_on'] = self.sort_on = self.sort_on.split('header-')[1]
+
+        kwargs['sort_order'] = self.sort_order = self.request.get('sort_order', self.sort_order)
+
+
+        #overwrite options with search_options dict on tab
+        kwargs.update(self._search_options)
+
+        self.search(kwargs)
+
+    def search(self, kwargs):
+        catalog = getToolByName(self.context,'portal_catalog')
+        if IATTopic.providedBy(self.context):
+            contentsMethod = self.context.queryCatalog
+        else:
+            contentsMethod = self.context.getFolderContents
+
+        self.contents = catalog(path=dict(depth=self.depth, query='/'.join(self.context.getPhysicalPath())), **kwargs)
+        #when we're searching recursively we have to remove the current item if its in self.types
+        if self.depth != 1 and self.context.portal_type in self.types:
+            index = 0
+            current_path = '/'.join(self.context.getPhysicalPath())
+            for result in self.contents:
+                if current_path == result.getPath():
+                    self.contents = list(self.contents)
+                    self.contents.pop(index)
+                    #removing from LazyMap doesn't work
+                    #self.contents._len -= 1
+                    #self.contents.actual_result_count -= 1
+                    #res = self.contents._data.pop(index)
+                index +=1
+
+        self.len_results = len(self.contents)
 
     @property
     @instance.memoize
     def batch(self):
+        pagesize = self.pagesize
         b = Batch(self.contents,pagesize=self.pagesize,pagenumber=self.pagenumber)
         return b
 
-    def show_search_results(self):
-        if self.request.has_key('searchable_text'):
-            searchable_text = self.request.get('searchable_text')
-            return bool(len(searchable_text))
-        return False
+class SolrListingView(ListingView):
+    
+    def build_query(self):
+        return self.search_util.buildQuery(**self._search_options)
 
+    def search(self, kwargs={}):
+        self.search_util = queryUtility(ISearch)
+        parameters = {}
+        query = self.build_query()
+        flares = self.search_util(query, **parameters)
+        
+        self.contents = [PloneFlare(f) for f in flares]
 
 class GenericListing(BaseListingView):
     """ uses the tab id for self.types. Good for testing or fallback  """
