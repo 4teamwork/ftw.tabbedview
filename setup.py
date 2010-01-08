690e3622df126234c9574aa2437c3c2f865f1827
jonas.baumann <jonas.baumann@c579a809-fee1-0310-b81e-b0ae3483ba1b>
2010-01-08 16:01:17 +0000

    egg cleanup
    git-svn-id: https://svn.4teamwork.ch/repos/ftw/ftw.tabbedview/trunk@32889 c579a809-fee1-0310-b81e-b0ae3483ba1b

--- setup.py
+++ setup.py
@@ -1,41 +1,45 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = open('ftw/tabbedview/version.txt').read().strip()
+maintainer = 'Victor Baumann'
 
 setup(name='ftw.tabbedview',
       version=version,
-      description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="" + \
+          ' (Maintainer %s)' % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='GPL',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      url='http://psc.4teamwork.ch/4teamwork/ftw/ftw.tabbedview/',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          'collective.js.jquery',
-          'plone.app.registry',
-          'ftw.table',
-          'ftw.js.statusmessages',
-          'ftw.js.globals'
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'collective.js.jquery',
+        'plone.app.registry',
+        'ftw.table',
+        'ftw.js.statusmessages',
+        'ftw.js.globals'
+        # -*- Extra requirements: -*-
+        ],
       extras_require = {
-      	            'jqueryui' : ['collective.js.jqueryui'],
-      	            'jquerytools' : ['plone.app.jquerytools'],
-      },
+        'jqueryui' : ['collective.js.jqueryui'],
+        'jquerytools' : ['plone.app.jquerytools'],
+        },
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = opengever
       """,
       )
