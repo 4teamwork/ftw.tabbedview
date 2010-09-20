937bbdf17dd3ebc3f0e01dd3ef045df81e4dfd54
Jonas Baumann <jone@jone.ch>
2010-09-20 20:03:51 +0200

    conly show searchbox viewlet on a tabbed view

--- ftw/tabbedview/browser/searchbox.py
+++ ftw/tabbedview/browser/searchbox.py
@@ -1,6 +1,23 @@
 from plone.app.layout.viewlets import common
+from zope.component import getMultiAdapter
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from ftw.tabbedview.browser.tabbed import TabbedView
 
 
 class SearchBox(common.ViewletBase):
     index = ViewPageTemplateFile('searchbox.pt')
+
+    def available(self):
+        plone = getMultiAdapter((self.context, self.request),
+                                name=u'plone_context_state')
+        try:
+            view = self.context.restrictedTraverse(plone.view_template_id())
+        except AttributeError:
+            return False
+        return isinstance(view, TabbedView)
+
+    def render(self):
+        if not self.available():
+            return ''
+        else:
+            return common.ViewletBase.render(self)
