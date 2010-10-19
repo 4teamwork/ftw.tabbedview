b4229549fa9eaeb204e1fcce6cb6a8049889a4ea
Rok Garbas <rok@garbas.si>
2010-10-19 12:58:23 +0200

    removing collective.js.jqueryui as dependency

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(name='ftw.tabbedview',
         'setuptools',
         'ftw.table',
         'plone.app.registry',
-        'collective.js.jqueryui'
         # -*- Extra requirements: -*-
         ],
       extras_require = {
