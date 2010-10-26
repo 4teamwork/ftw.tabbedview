365bc136ede206d9ea7840347f6bfffff7daddb3
Jonas Baumann <jone@jone.ch>
2010-10-26 13:09:40 +0200

    Changed the minimum pagesize to 1. A small pagesize is necessary for easy   testing.

--- ftw/tabbedview/interfaces.py
+++ ftw/tabbedview/interfaces.py
@@ -5,6 +5,6 @@ from zope.interface import Interface
 class ITabbedView(Interface):
     """A type for collaborative spaces."""
 
-    batch_size = schema.Int(title=u"Batch Size", min=10, default=50)
+    batch_size = schema.Int(title=u"Batch Size", min=1, default=50)
     timeout = schema.Int(title=u"Timeout for ajax Requests in ms",
                         min=500, default=5000)
