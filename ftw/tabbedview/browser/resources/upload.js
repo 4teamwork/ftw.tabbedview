5e7f22d05c7693d7ed54cc7190ff24de31df81f4
philippe.gross <philippe.gross@4teamwork.ch>
2012-03-19 11:13:58 +0100

    Add some adjustments to the javascript, to uploadbox hiding.

--- ftw/tabbedview/browser/resources/upload.js
+++ ftw/tabbedview/browser/resources/upload.js
@@ -1,36 +1,36 @@
 jq(function(){
 
-   /* ----------------------------------
-       Added functionality to the upload box,
-       copied from collective.quickupload*/
+  /* ----------------------------------
+     Added functionality to the upload box,
+     copied from collective.quickupload*/
 
 
-    // workaround this MSIE bug :
-    // https://dev.plone.org/plone/ticket/10894
-    if (jQuery.browser.msie) jQuery("#settings").remove();
-    var Browser = {};
-    Browser.onUploadComplete = function() {
-        window.location.reload();
-    };
+  // workaround this MSIE bug :
+  // https://dev.plone.org/plone/ticket/10894
+  if (jQuery.browser.msie) jQuery("#settings").remove();
+  var Browser = {};
+  Browser.onUploadComplete = function() {
+    window.location.reload();
+  };
 
-    loadUploader = function() {
-      var ulContainer = jQuery('#uploadbox');
-      ulContainer.each(function(){
-        var uploadUrl =  jQuery('.uploadUrl', this).val();
-        var uploadData =  jQuery('.uploadData', this).val();
-        var UlDiv = jQuery(this);
-        jQuery.ajax({
-          type: 'GET',
-          url: uploadUrl,
-          data: uploadData,
-          dataType: 'html',
-          contentType: 'text/html; charset=utf-8',
-            success: function(html) {
-              UlDiv.html(html);
-            }
-        });
+  loadUploader = function() {
+    var ulContainer = jQuery('#uploadbox');
+    ulContainer.each(function(){
+      var uploadUrl =  jQuery('.uploadUrl', this).val();
+      var uploadData =  jQuery('.uploadData', this).val();
+      var UlDiv = jQuery(this);
+      jQuery.ajax({
+        type: 'GET',
+        url: uploadUrl,
+        data: uploadData,
+        dataType: 'html',
+        contentType: 'text/html; charset=utf-8',
+        success: function(html) {
+          UlDiv.html(html);
+        }
       });
-    };
+    });
+  };
   loadUploader();
 
   var uploadbox = jq('#uploadbox');
@@ -38,22 +38,28 @@ jq(function(){
 
   jq('.tabbedview_view').live('dragover', function(event){
     uploadbox.show();
+    jq('.qq-upload-button').hide();
+    jq('.pannelHeader').hide();
+    jq('#label-upload').hide();
   });
 
   jq('.tabbedview_view').live('dragleave', function(event){
-      uploadbox.hover(
+    uploadbox.hover(
       function () {
         /* Do nothing*/
       },
       function () {
-        uploadbox.hide();
+        var uploading = eval('xhr_' + uploadbox.find('.main-uploader').attr('id')).isUploading();
+        if (! uploading){
+          uploadbox.hide();
+        }
       }
     );
   });
 
   PloneQuickUpload.onAllUploadsComplete = function(event){
     uploadbox.hide();
+    jq('#label-upload').hide();
     tabbedview.reload_view();
   };
-
 });
