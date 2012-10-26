66f3887b36ba62b3c990e954f97c33ec23daf225
Jonas Baumann <jone@jone.ch>
2012-10-26 17:59:38 +0200

    Basic support for using the tabbed view anonymously.
    ExtJS is not supported and automatically disabled for anonymous users.

--- ftw/tabbedview/browser/config.py
+++ ftw/tabbedview/browser/config.py
@@ -1,6 +1,7 @@
 from Products.Five.browser import BrowserView
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
+import AccessControl
 
 
 class TabbedviewConfigView(BrowserView):
@@ -8,6 +9,14 @@ class TabbedviewConfigView(BrowserView):
     def extjs_enabled(self):
         """Returns `True` if extjs is enabled.
         """
+
+        user = AccessControl.getSecurityManager().getUser()
+        if user == AccessControl.SpecialUsers.nobody:
+            # ExtJS is not supported for anonymous users, since we are not
+            # able to store the state. Things such as sorting would not even
+            # work in the session.
+            return False
+
         registry = getUtility(IRegistry)
         return registry['ftw.tabbedview.interfaces.'
                         'ITabbedView.extjs_enabled']
