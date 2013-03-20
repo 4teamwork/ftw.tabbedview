91f6104be8714cf425f43cd96911987a42620d51
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2013-03-20 15:25:58 +0100

    Use removeAttr (jQuery 1.7.x)

--- ftw/tabbedview/browser/resources/tabbedview.js
+++ ftw/tabbedview/browser/resources/tabbedview.js
@@ -418,12 +418,13 @@ load_tabbedview = function(callback) {
 
   tabbedview.view_container.bind('reload', function() {
     //hide or show filter box
+    console.info($('.tabbedview-tabs li a.selected.searchform-hidden').length);
     if($('.tabbedview-tabs li a.selected.searchform-hidden').length){
       tabbedview.searchbox.closest('.tabbedview_search').addClass('disabledSearchBox');
       tabbedview.searchbox.closest('.tabbedview_search input').attr('disabled', 'disabled');
     }else{
       tabbedview.searchbox.closest('.tabbedview_search').removeClass('disabledSearchBox');
-      tabbedview.searchbox.closest('.tabbedview_search input').attr('disabled', '');
+      tabbedview.searchbox.closest('.tabbedview_search input').removeAttr('disabled');
     }
 
 
