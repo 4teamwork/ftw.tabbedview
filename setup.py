e09ef9739199f16da1b6039afc36173bfbec57ca
David Erni <david.erni@4teamwork.ch>
2016-03-01 11:51:35 +0100

    Drop plone 4.1 support.

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(name='ftw.tabbedview',
 
       classifiers=[
         'Framework :: Plone',
-        'Framework :: Plone :: 4.1',
         'Framework :: Plone :: 4.2',
         'Framework :: Plone :: 4.3',
         'Programming Language :: Python',
